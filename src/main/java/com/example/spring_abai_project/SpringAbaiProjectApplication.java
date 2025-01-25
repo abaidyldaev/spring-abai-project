0fda0f792a217c892623863a60b7ad25348f4b12
admin <dyldaab_aehit@students.vizja.pl>
2025-01-25 09:44:57 +0100

    Initial commit

---
+++ src/main/java/com/example/spring_abai_project/SpringAbaiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_abai_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAbaiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAbaiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_abai_project/SpringAbaiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_abai_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAbaiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
